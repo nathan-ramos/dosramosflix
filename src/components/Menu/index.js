ded5b73be3fba029a63530a93241af87d070a15a
Nathan Ramos <nathancostaand1@hotmail.com>
2020-07-28 22:17:52 -0300

    Setup inicial do projeto

---
+++ src/components/Menu/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Logo from '../../assets/img/Logo.png';
+import './Menu.css';
+//import ButtonLink from './components/ButtonLink';
+import Button from '../Button';
+
+
+function Menu(){
+    return (
+        <nav className="Menu">
+            <a href="/">
+                <img className="Logo" src={Logo} alt="dosRamosFlix logo"/>
+            </a>
+
+            <Button as="a" className="ButtonLink" href="/">
+                Novo video
+            </Button>
+        </nav>
+    );
+}
+
+export default Menu;
